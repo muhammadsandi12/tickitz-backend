56324c83e818ec828eb3454dc4ca806f0d066a19
muhammadsandi12 <muhammadsandimaulana12@gmail.com>
2022-09-09 18:14:16 +0700

    update 1

--- routes/premiereRoute.js
+++ routes/premiereRoute.js
@@ -3,7 +3,6 @@ const premiereController = require('../controller/premiereController')
 const router = express.Router()
 const upload = require('../helper/multer')
 
-// router.get('/', moviesController.getAllMovies)
 router.post('/',upload.single('logo_premiere'),premiereController.add)
 
 module.exports =router
